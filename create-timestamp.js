68765082da86156cbea2d451f1770964d12176b2
Bryan Sills <bryan@bryansills.com>
2019-10-22 19:15:39 -0500

    create simple project that creates a file with a timestamp

---
+++ create-timestamp.js
@@ -0,0 +1,8 @@
+const fs = require("fs")
+const path = require("path")
+
+const directory = path.join(__dirname, `/timestamps`)
+!fs.existsSync(directory) && fs.mkdirSync(directory)
+
+const timestamp = Date().toString().replace(/[\W_]+/g, "")
+fs.writeFileSync(path.join(__dirname, `/timestamps/${timestamp}.txt`), timestamp)
