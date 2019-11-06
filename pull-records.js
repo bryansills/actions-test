4a622b24e3242500bdf4361127fc842feefcf31a
Bryan Sills <bryan@bryansills.com>
2019-11-05 18:52:13 -0600

    Use a while loop to fetch all the dat

--- pull-records.js
+++ pull-records.js
@@ -5,16 +5,33 @@ const fetch = require("node-fetch")
 const directory = path.join(__dirname, `/records`)
 !fs.existsSync(directory) && fs.mkdirSync(directory)
 
-fetch(
-    "https://api.airtable.com/v0/appggFxxGcngfBBdz/Main?maxRecords=50&view=Grid%20view",
-    {
-        "headers": {
-            "Authorization": "Bearer keyR7Yl2PVWG9daU9"
+const fetchPage = async (offset) => {
+    const offsetQueryParam = !!offset ? `&offset=${offset}` : ""
+    const networkResult = await fetch(
+        `https://api.airtable.com/v0/appggFxxGcngfBBdz/Main?view=Grid%20view${offsetQueryParam}`,
+        {
+            "headers": {
+                "Authorization": "Bearer keyR7Yl2PVWG9daU9"
+            }
         }
+    )
+    return await networkResult.json()
+}
+
+const fetchAllData = async () => {
+    let records = []
+    let offset = ""
+
+    while (offset != null) {
+        const page = await fetchPage(offset)
+        records = [...records, ...page.records]
+        offset = page.offset
     }
-).then(response => {
-    return response.json()
-}).then(jsonRes => {
+
+    return records
+}
+
+fetchAllData().then(jsonRes => {
     const timestamp = Date().toString().replace(/[\W_]+/g, "")
     fs.writeFileSync(path.join(__dirname, `/timestamps/${timestamp}.json`), JSON.stringify(jsonRes, null, 4))
 })
