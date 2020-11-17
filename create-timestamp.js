5dcb9338eb5909e5d6acd8763cdde378e2f9d1cc
Bryan Sills <bsills@bignerdranch.com>
2020-11-17 10:05:04 -0600

    Now actually don't make changes

--- create-timestamp.js
+++ create-timestamp.js
@@ -4,5 +4,5 @@ const path = require("path")
 const directory = path.join(__dirname, `/timestamps`)
 !fs.existsSync(directory) && fs.mkdirSync(directory)
 
-// const timestamp = Date().toString().replace(/[\W_]+/g, "")
-// fs.writeFileSync(path.join(__dirname, `/timestamps/${timestamp}.txt`), timestamp)
+const timestamp = Date().toString().replace(/[\W_]+/g, "")
+fs.writeFileSync(path.join(__dirname, `/timestamps/${timestamp}.txt`), timestamp)
